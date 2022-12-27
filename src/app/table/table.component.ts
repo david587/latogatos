95446e5d2f38d4c35cdbfbc91807365df253be22
David <davidbarath08@gmail.com>
2022-12-27 15:34:52 +0100

    félkész

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -19,8 +19,8 @@ export class TableComponent {
     {nev: "Székesfehérvár	", megye: "Fejér", Terulet: "170.89km", Nepesseg:"95545"},
     {nev: "Szombathely	", megye: "Vas", Terulet: "97,5 km", Nepesseg:"2134"},
     {nev: "Sopron	", megye: "Győr-Moson-Sopron", Terulet: "120.89km", Nepesseg:"3544"},
-    {nev: "Zalaegerszeg	", megye: "Zala", Terulet: "70.89km", Nepesseg:"34545"},
-    {nev: "Sárvár	", megye: "Vas", Terulet: "98.9km", Nepesseg:"9545"},
+    {nev: "Zalaegerszeg	", megye: "Zala", Terulet: "70.km98", Nepesseg:"34545"},
+    {nev: "Sárvár	", megye: "Vas", Terulet: "98.9km", Nepesseg:"9565"},
     {nev: "Lenti	", megye: "Zala", Terulet: "23.8km", Nepesseg:"8545"},
     {nev: "Zalakaros	", megye: "Zala", Terulet: "10.89km", Nepesseg:"545"}
   ];
